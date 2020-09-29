810cdc75f19f2e4dad05dc65ff6fd182bd0317da
phanindra naidu <phanindranaidu222@gmail.com>
2020-09-29 10:29:31 +0530

    CReated ProductModel and IndexPage

---
+++ PhaniProject/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PhaniProject.Models
+{
+    public class Product
+    {
+        public int id { get; set; }
+
+        public string name { get; set; }
+
+        public float price { get; set; }
+        
+        public string ImageName { get; set; }
+    }
+}
